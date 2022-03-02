6a016fcfd545b6fea50fe292bfa89fefd2c35acb
Kyle <kylegwapo04@gmail.com>
2022-03-02 23:27:25 +0800

    mas better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/api", studentRouter);
 app.use("/api", adminRouter);
 app.use("/api", appointmentRouter);
 app.use("/api", RequestRouter);
-if (proces.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
